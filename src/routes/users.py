4ae3d17cf4a1567eae03bc7bd37c8a3cdf031ff3
loristron <loremmiranda@gmail.com>
2023-05-03 13:25:16 -0400

    Implementação de cadastro de usuário

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, jsonify, make_response
+from flask import Blueprint, jsonify, make_response, request
 from flasgger import swag_from
 from flask_httpauth import HTTPTokenAuth
 from conf.passwords import CREDS
 from services.users_service import dbConnector
+from werkzeug.security import generate_password_hash, check_password_hash
+from tools.tools import is_valid_email, is_valid_date, convert_string_to_datetime
 import json
 
 token_auth = HTTPTokenAuth(scheme='Bearer')
@@ -21,4 +23,47 @@ users = Blueprint('users', __name__, url_prefix='/api/users')
 @swag_from('docs/get_users.yaml') 
 def get_users():
     data = json.loads(database.get_all_users())
-    return make_response(jsonify(data), 200)   
+    return make_response(jsonify(data), 200)
+
+@users.route('/create',  methods=['POST'])
+@token_auth.login_required
+@swag_from('docs/insert_user.yaml') 
+def insert_user():
+    try:
+        name = request.form['name'].strip().replace("'", '`').title()
+        username = request.form['username'].lower().strip()
+        email = request.form['email'].strip()
+        phone = request.form['phone'].strip().replace('+55', '').replace(')', '').replace("(", '')
+        date_of_birth = request.form['date_of_birth'].strip()
+        password = request.form['password'].strip()
+
+        ## Verificação de email 
+        if not is_valid_email(email=email):
+            return make_response(jsonify({'error': 'Enderço de email inválido'}))
+        
+        if not is_valid_date(date_string=date_of_birth):
+            return make_response(jsonify({'error': 'Data de nascimento inválida'}))
+
+        ## Verificação se usuário já existe 
+        query_verifica = f"SELECT * FROM users WHERE username = '{username}' OR email = '{email}'"
+        verifica_db = json.loads(database.make_select(query_verifica))
+        if verifica_db.get('ok'):
+            if len(verifica_db.get('data')) > 0:
+                return make_response(jsonify({'data': 'Email ou nome de usuário já cadastrados'}))
+            
+        date_of_birth = convert_string_to_datetime(date_string=date_of_birth)
+        hashed_password = generate_password_hash(password)
+        data = json.loads(database.insert_user(
+            name=name, 
+            username=username,
+            email=email, 
+            phone=phone,
+            date_of_birth=date_of_birth,
+            hash_password=hashed_password
+            ))
+        if data.get('ok'):
+            return make_response(jsonify(data), 201)
+        else:
+            return make_response(jsonify({'error': 'Algo deu errado!'}), 400)
+    except Exception as e:
+        return make_response(jsonify({'error': str(e), 'ok': False}, 500))
--- src/services/users_service.py
+++ src/services/users_service.py
@@ -2,6 +2,7 @@ import pymysql
 from google.cloud.sql.connector import Connector
 import json
 from conf.passwords import CREDS
+from datetime import datetime
 
 class dbConnector():
 
@@ -24,3 +25,36 @@ class dbConnector():
             return json.dumps({'data': result, 'ok': True}, default=str)
         except Exception as e:
             return json.dumps({'erorr': e, 'ok': False}, default=str)
+        
+    def make_select(self, sql: str):
+        try:
+            cur = self.cnx.cursor()
+            cur.execute(sql)
+            result = cur.fetchall()
+            return json.dumps({'data': result, 'ok': True}, default=str)
+        except Exception as e:
+            return json.dumps({'erorr': e, 'ok': False}, default=str)
+        finally:
+            cur.close()
+        
+    def insert_user(self, name: str, username: str, email: str, phone: str, hash_password: str, date_of_birth: datetime):
+        try:
+            sql =f"""
+                INSERT INTO `users` (`name`, `username`, `email`, `phone`, `hash_password`, `date_of_birth`) 
+                VALUES ('{name}', '{username}', '{email}', '{phone}', '{hash_password}', '{date_of_birth}');
+            """ 
+            cur = self.cnx.cursor()
+            cur.execute(sql)
+            self.cnx.commit()
+            return json.dumps({'data': {
+                'name': name, 
+                'username': username, 
+                'email': email,
+                'phone': phone, 
+                'password': hash_password, 
+                'date_of_birth': date_of_birth
+            }, 'ok': True}, default=str)
+        except Exception as e:
+            return json.dumps({'erorr': str(e), 'ok': False}, default=str)
+        finally:
+            cur.close()
