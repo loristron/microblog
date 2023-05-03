1b1607fa6417dd74fdece3ead971185a6e521c92
loristron <loremmiranda@gmail.com>
2023-05-03 11:09:36 -0400

    API Flask com Swagger

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask
+from flasgger import Swagger
+from conf.swagger_template import template
+from routes.users import users
+
+
+app = Flask(__name__)
+app.register_blueprint(users)
+
+# Generate key using (uuid.uuid4().hex)
+secret_key = os.environ['APP_SECRET_KEY']
+app.secret_key = secret_key
+app.config['SWAGGER'] = {
+    'title': 'API Microblog',
+    'uiversion': 3,
+    "specs_route": "/"
+}
+
+swagger = Swagger(app, template=template)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
